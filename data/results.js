0856d7c10b1f6825af1cf9eab60075255d33172a
Michael Wehrley <michaelwehrley@users.noreply.github.com>
2020-10-14 13:09:55 -0500

    example of using JSON here

---
+++ data/results.js
@@ -0,0 +1,24 @@
+// Example of your data in a "database" format.  We call this JSON.
+
+var results = {
+  york: {
+    brady: {
+      place: 26,
+      minutes: 17,
+      seconds: 00,
+    },
+    eamon: {
+      place: 2,
+      minutes: 13,
+      seconds: 33,
+    },
+    liam: {
+      place: 6,
+      minutes: 14,
+      seconds: 26,
+    }
+  },
+  morton: {
+
+  }
+};
---
+++ data/users.js
@@ -0,0 +1,11 @@
+// you can ignore
+
+var users = [
+  { id: "brady" },
+  { id: "eamon" },
+  { id: "leo" },
+  { id: "liam" },
+  { id: "lou" },
+  { id: "owen" },
+  { id: "roscoe" }
+];
